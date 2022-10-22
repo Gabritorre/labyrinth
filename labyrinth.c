67dc5868d77b122d4a963e882749ea47a64b7f7f
Gabritorre <gabritorredev@gmail.com>
2022-10-22 15:43:25 +0200

    Save and print map

--- labyrinth.c
+++ labyrinth.c
@@ -7,6 +7,39 @@ void title() {
 	printf(" -------------------\n");
 }
 
+void print_map() {
+	FILE *ptr;
+
+	ptr = fopen("maps.txt", "r");
+	if (NULL == ptr) {
+		printf("dio scroto");
+	}
+	char map[10][19];
+	char character = fgetc(ptr);
+
+	for (int row = 0; character != EOF; row++){
+		for (int column = 0; character != EOF; column++){
+			if (character == '\n'){
+				character = fgetc(ptr);
+				break;
+			}
+			map[row][column] = character;
+			printf("%c", character);
+			character = fgetc(ptr);
+		}
+	}
+
+	fclose(ptr);
+	printf("\n\nmappa:\n\n");
+	for (int row = 0; row<10; row++){
+		printf("\n");
+		for (int column = 0; column<19; column++){
+			printf("%c", map[row][column]);
+		}
+	}
+
+}
+
 int main() {
 
 	bool play = true;
@@ -22,6 +55,8 @@ int main() {
 
 		if (game_mode[0] == '1' && game_mode[1] == 0) {
 			printf("hai selezionato modalita' interattiva\n");
+			print_map();
+			printf("\n\n");
 		}
 
 		else if (game_mode[0] == '2' && game_mode[1] == 0) {
