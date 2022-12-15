f0a37075d30fec5048416f4039ada161bd4504b1
Gabritorre <gabritorredev@gmail.com>
2022-12-16 00:22:04 +0100

    Reduce drastically the probability of going in a endless loop

--- iap/lib/game_mode/ai.h
+++ iap/lib/game_mode/ai.h
@@ -15,8 +15,8 @@ int run_ghost(struct Map map_info, char map[map_info.row][map_info.column], char
 		return 0;
 	}
 	//se vicolo ceco
-	if (next_row == 0 || next_row == map_info.row || next_column == 0 || next_column == map_info.column) {
-		return -9999;
+	if (next_row < 0 || next_row >= map_info.row || next_column < 0 || next_column >= map_info.column) {
+		return 9999;
 	}
 
 /*	continua a muoverti finchè trovi degli ostacoli dove stai guardando*/
@@ -90,10 +90,19 @@ void go_around_wall(struct Map* map_info, char map[map_info->row][map_info->colu
 		ghost2_steps = run_ghost(*map_info, map, SUD, vert_value, horiz_value);
 		move = NORD;
 		better_path = ghost1_steps;
-		if(ghost1_steps > ghost2_steps || target_row > map_info->player_row) {
+		if(ghost1_steps > ghost2_steps) {
+			better_path = ghost2_steps;
+			move = SUD;
+			if (ghost1_steps != 9999 && target_row < map_info->player_row){
+				better_path = ghost1_steps;
+				move = NORD;
+			}
+		}
+		if (ghost2_steps != 9999 && target_row > map_info->player_row){
 			better_path = ghost2_steps;
 			move = SUD;
 		}
+		printf("better_move di v %c\n", move);
 
 	}
 	else if (direction == 'h') {
@@ -107,11 +116,19 @@ void go_around_wall(struct Map* map_info, char map[map_info->row][map_info->colu
 		ghost2_steps = run_ghost(*map_info, map, EST, vert_value, horiz_value);
 		move = OVEST;
 		better_path = ghost1_steps;
-		if(ghost1_steps > ghost2_steps || target_col > map_info->player_column) {
+		if(ghost1_steps > ghost2_steps) {
+			better_path = ghost2_steps;
+			move = EST;
+			if (ghost1_steps != 9999 && target_col < map_info->player_column){
+				better_path = ghost1_steps;
+				move = OVEST;
+			}
+		}
+		if (ghost2_steps != 9999 && target_col > map_info->player_column){
 			better_path = ghost2_steps;
 			move = EST;
 		}
-
+		printf("better_move di h %c\n", move);
 	}
 
 	//eseguo il percorso deciso per raggirare il muro
@@ -122,14 +139,34 @@ void go_around_wall(struct Map* map_info, char map[map_info->row][map_info->colu
 	}
 }
 
+int inspect_item(struct Map *map_info, char map[map_info->row][map_info->column], int item_row, int item_col) {
+	int counter = 0;
+
+	if(map[item_row + 1][item_col] == WALL) {
+		counter++;
+	}
+	if(map[item_row - 1][item_col] == WALL) {
+		counter++;
+	}
+	if(map[item_row][item_col + 1] == WALL) {
+		counter++;
+	}
+	if(map[item_row][item_col - 1] == WALL) {
+		counter++;
+	}
+	return counter;
+}
+
 /* Sposta la posizione del personaggio verso un determinato target che gli viene passato come parametro
  * parametri: le informazioni della mappa, la mappa, la stringa contenenti i passi fatti dal personaggio, la lunghezza di quest'utlima stringa, la colonna del target, la riga del target, il punteggio
+ * ritorna vero se ha interrotto forzatamente il raggiungimento del target, ritorna falso se lo ha raggiunto correttamente
  * */
-void goto_target(struct Map* map_info, char map[map_info->row][map_info->column], char** all_steps, int* all_steps_size, int target_col, int target_row, int *points, vector** tail) {
+bool goto_target(struct Map* map_info, char map[map_info->row][map_info->column], char** all_steps, int* all_steps_size, int target_col, int target_row, int *points, vector** tail) {
 
 	char move; //contine la mossa da fare
 	char target_direction = 'h'; // serve a capire da quale direzione (verticale od orizzontale) si può accedere all'uscita
 	char left_target = '?', right_target = '?';
+	int panic_counter = 0;
 
 	if (target_col - 1 >= 0) {
 		left_target = map[target_row][target_col - 1];
@@ -151,15 +188,24 @@ void goto_target(struct Map* map_info, char map[map_info->row][map_info->column]
 		else if (target_row == map_info->player_row) {
 			target_direction = 'h';
 		}
+
 	}
+	//ignora il target
+	if (inspect_item(map_info, map, target_row, target_col) >= 3) {
+		target_direction = 'i';
+	}
+
 /*	printf("target_direction %c\n", target_direction);*/
 
 	//finche il personaggio non trova percorso retto senza ostacoli verso il target
-	while (!green_light_to_target(map_info, map, target_col, target_row, target_direction)) {
+	while (!green_light_to_target(map_info, map, target_col, target_row, target_direction) && panic_counter <= map_info->row + map_info->column) {
 /*		printf("target_col %d\n", target_col);*/
 /*		printf("target_row %d\n", target_row);*/
+		panic_counter++;
 		if (target_direction == 'h') { // vogliamo far corrispondere la riga del giocatore con quella dell'uscita
+			printf("accesso orizzontale\n");
 			if (target_row == map_info->player_row){
+				printf("sono nella stessa riga\n");
 				if (target_col > map_info->player_column) {
 					move = EST;
 				}
@@ -169,10 +215,13 @@ void goto_target(struct Map* map_info, char map[map_info->row][map_info->column]
 				if(!run_move(map_info, map, move, points, all_steps, all_steps_size, true, tail)) {
 					go_around_wall(map_info, map, 'v', move, all_steps, all_steps_size, points, tail, target_row, target_col);
 					//una volta raggirato il muro esegue la mossa che mi avvicina al target
+					printf("mi muovo a %c\n", move);
 					run_move(map_info, map, move, points, all_steps, all_steps_size, true, tail);
 /*					print_map(map_info, map);*/
 /*					printf("\nsequenza: %s\n", *all_steps);*/
 				}
+				print_map(map_info, map);
+				printf("\nsequenza: %s\n", *all_steps);
 				continue;
 			}
 			else if (target_row > map_info->player_row) {
@@ -185,6 +234,7 @@ void goto_target(struct Map* map_info, char map[map_info->row][map_info->column]
 			}
 			if(!run_move(map_info, map, move, points, all_steps, all_steps_size, true, tail)) {
 				go_around_wall(map_info, map, 'h', move, all_steps, all_steps_size, points, tail, target_row, target_col);
+				printf("vado a %d\n", move);
 				run_move(map_info, map, move, points, all_steps, all_steps_size, true, tail);
 			}
 		}
@@ -203,6 +253,8 @@ void goto_target(struct Map* map_info, char map[map_info->row][map_info->column]
 /*					print_map(map_info, map);*/
 /*					printf("\nsequenza: %s\n", *all_steps);*/
 				}
+				print_map(map_info, map);
+				printf("\nsequenza: %s\n", *all_steps);
 				continue;
 			}
 			else if (target_col > map_info->player_column) {
@@ -218,8 +270,15 @@ void goto_target(struct Map* map_info, char map[map_info->row][map_info->column]
 				run_move(map_info, map, move, points, all_steps, all_steps_size, true, tail);
 			}
 		}
+		else if (target_direction == 'i') {
+			return false;
+		}
 		print_map(map_info, map);
 		printf("\nsequenza: %s\n", *all_steps);
+	}
+
+	if(panic_counter >= map_info->row + map_info->column){
+		return true;
 	}
 
 	// dopo essersi allineati con il target è possibile andare dritto verso di esso
@@ -262,6 +321,7 @@ void goto_target(struct Map* map_info, char map[map_info->row][map_info->column]
 		print_map(map_info, map);
 		printf("\nsequenza: %s\n", *all_steps);
 	}
+	return false;
 }
 
  /* controlla se nel percorso per raggiungere l'uscita si trovano delle monete abbastanza vicine per essere raggiunte, una volta finite le monete raggiungibili punta all'uscita*/
@@ -270,21 +330,23 @@ void coin_exit_algorithm(struct Map* map_info, char map[map_info->row][map_info-
 	int column = map_info->player_column;
 	int further_column = map_info->column - 1; // colonna più lontana da scannerizzare per cercare delle monete
 	bool deep_inspect = false; //nel caso in cui il personaggio e l'uscita siano allineati sin dalla partenza allora cerca delle monete nelle colonne adiacenti a qualla per raggiungere l'uscita
-
-	if (map_info->player_column < map_info->exit_column) {
-		column = map_info->player_column - 9;
-	}
-	else if (map_info->player_column > map_info->exit_column) {
-		column = map_info->player_column + 9;
+	if (map_info->player_column > map_info->exit_column) {
+		column = map_info->player_column + 10;
+		if(map_info->player_column - 10 > 0){
+			further_column = map_info->player_column - 10;
+		}
+		else {
+			further_column = 0;
+		}
 	}
 	else{
-		column = map_info->player_column - 9;
-		if(map_info->player_column + 9 < map_info->column - 1){
-			further_column = map_info->player_column + 9;
+		column = map_info->player_column - 10;
+		if(map_info->player_column + 10 <= map_info->column){
+			further_column = map_info->player_column + 10;
 		}
-		deep_inspect = true;
 	}
 
+	deep_inspect = true;
 	//evito di uscire dai limiti della mappa
 	if(column <= 0) {
 		column = 1;
@@ -296,12 +358,23 @@ void coin_exit_algorithm(struct Map* map_info, char map[map_info->row][map_info-
 	//ricerca delle monete facendo il seguente percorso: dall'alto al basso -> cambio colonna -> dal basso all'alto
 	//-> cambio colonna -> dall'alto al basso ecc...
 	int start = 0, end = map_info->row - 1;
-	while(column != map_info->exit_column || deep_inspect) {
+	bool panic = false;
+	while(deep_inspect) {
+		if (panic){
+			break;
+		}
 		int row = start;
 		while(row != end) {
-			if(map[row][column] == BONUS_POINTS) {
-				printf("MONETA IN %d %d", row, column);
-				goto_target(map_info, map, all_steps, all_steps_size, column, row, points, tail);
+			if (panic){
+				break;
+			}
+			if(map[row][column] == DRILL){
+				printf("trapano in %d %d\n", row, column);
+				panic = goto_target(map_info, map, all_steps, all_steps_size, column, row, points, tail);
+			}
+			else if(map[row][column] == BONUS_POINTS) {
+				printf("\t\tMONETA IN %d %d\n", row, column);
+				panic = goto_target(map_info, map, all_steps, all_steps_size, column, row, points, tail);
 			}
 			if(row < end){
 				row++;
@@ -310,17 +383,15 @@ void coin_exit_algorithm(struct Map* map_info, char map[map_info->row][map_info-
 				row --;
 			}
 		}
-		if (column < map_info->exit_column || deep_inspect) {
+		if (column < further_column) {
 			column += 1;
 		}
 		else {
 			column -= 1;
 		}
-		if (deep_inspect) {
-			//if (column == further_column || map[map_info->player_row][column] == WALL) {
-			if (column == further_column) {
-				deep_inspect = false;
-			}
+
+		if (column == further_column) {
+			deep_inspect = false;
 		}
 		// scambio start con end
 		int temp = end;
@@ -329,6 +400,7 @@ void coin_exit_algorithm(struct Map* map_info, char map[map_info->row][map_info-
 	}
 
 	//raggiungimento dell'uscita
+	printf("\n\n\t\t vado all'uscita");
 	goto_target(map_info, map, all_steps, all_steps_size, map_info->exit_column, map_info->exit_row, points, tail);
 }
 
